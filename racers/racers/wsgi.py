ea94518fb681077350a7e45171ecaed68c54d204
GeorgVerg <115580431+GeorgVerg@users.noreply.github.com>
2025-04-20 17:17:08 +0300

    Update wsgi.py REMOVED WRONG PORTS

--- racers/racers/wsgi.py
+++ racers/racers/wsgi.py
@@ -14,10 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'racers.settings')
 
 application = get_wsgi_application()
-
-
-# Make sure you bind to the correct port dynamically (for Render).
-from wsgiref.simple_server import make_server
-port = int(os.environ.get('PORT', 10000))  # Render sets the PORT environment variable
-httpd = make_server('', port, application)
-httpd.serve_forever()
